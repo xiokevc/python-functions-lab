a7d5b9bf9a82395cd7ae8f9fa7d66a638034b35e
xiokevc <xiokevc@gmail.com>
2025-09-03 14:01:48 -0400

    complete exercise 2

--- exercises.py
+++ exercises.py
@@ -25,6 +25,8 @@ print('Exercise 1:', calculate_area_triangle(10, 5))
 #
 # Define your function and call it to see the result.
 
+def simple_interest(principal, rate, time):
+    return (principal * rate * time) / 100
 
 print('Exercise 2:', simple_interest(1000, 5, 2))
 
